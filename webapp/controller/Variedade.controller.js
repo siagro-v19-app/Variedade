d951d530b1a323342d8baf0210e1c3ddc656598d
matusca96 <gomesloud@gmail.com>
2019-04-15 16:10:17 -0300

    Padronização.

--- webapp/controller/Variedade.controller.js
+++ webapp/controller/Variedade.controller.js
@@ -2,8 +2,10 @@ sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/m/MessageBox",
 	"sap/ui/model/json/JSONModel",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator",
 	"br/com/idxtecVariedade/services/Session"
-], function(Controller, MessageBox, JSONModel, Session) {
+], function(Controller, MessageBox, JSONModel, Filter, FilterOperator, Session) {
 	"use strict";
 
 	return Controller.extend("br.com.idxtecVariedade.controller.Variedade", {
@@ -15,6 +17,29 @@ sap.ui.define([
 
 			this.getOwnerComponent().setModel(oJSONModel, "model");
 			this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
+			
+			this.getModel().attachMetadataLoaded(function(){
+				var oFilter = new Filter("Empresa", FilterOperator.EQ, Session.get("EMPRESA_ID"));
+				var oView = this.getView();
+				var oTable = oView.byId("tableVariedade");
+				var oColumn = oView.byId("columnDescricao");
+				
+				oTable.sort(oColumn);
+				oView.byId("tableVariedade").getBinding("rows").filter(oFilter, "Application");
+			});
+		},
+		
+		filtraVariedade: function(oEvent){
+			var sQuery = oEvent.getParameter("query");
+			var oFilter1 = new Filter("Empresa", FilterOperator.EQ, Session.get("EMPRESA_ID"));
+			var oFilter2 = new Filter("Descricao", FilterOperator.Contains, sQuery);
+			
+			var aFilters = [
+				oFilter1,
+				oFilter2
+			];
+
+			this.getView().byId("tableVariedade").getBinding("rows").filter(aFilters, "Application");
 		},
 		
 		onRefresh: function(){
